3fb88b38e18b9575c552caf7c010ee8625e113c1
Gabriel Vion <gabriel.vion@lemonchiligames.com>
2025-11-22 11:54:42 -0300

    Fixed lifemeter last heart Ui Depth

--- soh/src/code/z_lifemeter.c
+++ soh/src/code/z_lifemeter.c
@@ -591,14 +591,14 @@ void HealthMeter_Draw(PlayState* play) {
             if ((ddHeartCountMinusOne < 0) || (i > ddHeartCountMinusOne)) {
                 if (curCombineModeSet != 2) {
                     curCombineModeSet = 2;
-                    Gfx_SetupDL_42Overlay(gfxCtx);
+                    Gfx_SetupDL_OverlayUIDepth(gfxCtx);
                     gDPSetCombineLERP(OVERLAY_DISP++, PRIMITIVE, ENVIRONMENT, TEXEL0, ENVIRONMENT, TEXEL0, 0, PRIMITIVE,
                                       0, PRIMITIVE, ENVIRONMENT, TEXEL0, ENVIRONMENT, TEXEL0, 0, PRIMITIVE, 0);
                 }
             } else {
                 if (curCombineModeSet != 4) {
                     curCombineModeSet = 4;
-                    Gfx_SetupDL_42Overlay(gfxCtx);
+                    Gfx_SetupDL_OverlayUIDepth(gfxCtx);
                     gDPSetCombineLERP(OVERLAY_DISP++, ENVIRONMENT, PRIMITIVE, TEXEL0, PRIMITIVE, TEXEL0, 0, PRIMITIVE,
                                       0, ENVIRONMENT, PRIMITIVE, TEXEL0, PRIMITIVE, TEXEL0, 0, PRIMITIVE, 0);
                 }
